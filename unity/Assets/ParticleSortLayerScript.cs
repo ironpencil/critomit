197c31b374d4ea094abab776452c6da16965be41
ironpencil <james.a.south@gmail.com>
2015-07-31 12:44:37 -0400

    Particles!!

---
+++ unity/Assets/ParticleSortLayerScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSortLayerScript : MonoBehaviour {
+
+    public string sortingLayerName = "Explosions";
+
+    public ParticleSystemRenderer psr;
+	// Use this for initialization
+	void Start () {
+        if (psr != null)
+        {
+            psr.sortingLayerName = sortingLayerName;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
